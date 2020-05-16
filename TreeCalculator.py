985e8e1d95f78a30caa7ba3a281fea98676f5106
vvkin <vkincur@gmail.com>
2020-05-16 17:36:59 +0300

    sorting algorithm added

--- TreeCalculator.py
+++ TreeCalculator.py
@@ -4,6 +4,7 @@ def is_operator(c):
         return True
     else: 
         return False
+        
 def parse_equation(equation):
     OPERATORS = ['+', '-', '/', '*', '(', ')']
     result = ''
@@ -14,5 +15,28 @@ def parse_equation(equation):
     while '  ' in result:
         result = result.replace('  ', ' ')
     return result.strip()
+    
+    def shunting_yard(sentence):
+        op = {'+': 1, '-':1, '*': 2, '/':2}
+        stack = []
+        formula = []
+        for token in sentence:
+            if token in op: 
+                while stack and stack[-1] != "(" and op[token] <= op[stack[-1]]:
+                    formula.append(stack.pop())
+                stack.append(token)
+            elif token == ")":
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    formula.append(x)
+            elif token == "(":
+                stack.append(token)
+            else:
+                formula.append(token)
+        while stack:
+            formula.append(stack.pop())
+        return formula
 
 filename = "code.txt"       
